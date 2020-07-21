6acfa00313eba0ca3893cdc7740062ca2801e49d
Devin Bidwell <dbidwell@intuituscyber.com>
2020-07-20 20:44:28 -0700

    created-404-page

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import headout from "../assets/imaheadout.jpg";
 
 export default function NotFound({ navbarHeight }) {
   const Container = styled.section`
@@ -11,27 +12,33 @@ export default function NotFound({ navbarHeight }) {
     justify-content: center;
     align-items: center;
     background: black;
-    div.card{
+    div.card {
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
       padding: 10rem;
+      padding-top: 1rem;
       background: white;
-      h2{
+      h2 {
         font-size: 2rem;
       }
-      p{
+      p {
         font-size: 1.5rem;
       }
-      a{
+      a {
         text-decoration: none;
         margin-top: 2rem;
         font-size: 1.25rem;
         color: white;
-        padding: ${`1.5rem ${1.5 * 16/9}rem`};
+        padding: ${`1.5rem ${(1.5 * 16) / 9}rem`};
         background: black;
-        border-radius: .75rem;
+        border-radius: 0.75rem;
+      }
+      img {
+        height: 15rem;
+        width: auto;
+        margin-bottom: 2rem;
       }
     }
   `;
@@ -39,6 +46,7 @@ export default function NotFound({ navbarHeight }) {
   return (
     <Container>
       <div className="card">
+        <img src={headout} alt="Error 404" />
         <h2>404 Oops!</h2>
         <p>The page you are looking for is not found!</p>
         <Link to="/">Take me home!</Link>
